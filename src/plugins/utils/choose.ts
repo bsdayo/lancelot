5afb2820bf0113935124724deb1683187a080031
b1acksoil <bs@blacksoil.top>
2022-03-20 14:57:25 +0800

    add prefix option for /choose

--- src/plugins/utils/choose.ts
+++ src/plugins/utils/choose.ts
@@ -5,8 +5,10 @@ export default function enableChoose(ctx: Context) {
   ctx
     .command('choose <...args>', '随机选择')
     .shortcut('帮我选', { fuzzy: true })
-    .action(({session}, ...args) => {
-      if (!args || args.length < 2) return reply(session) + '请输入至少两个选项！'
-      return '建议你选择' + args[randomInt(0, args.length - 1)]
+    .option('prefix', '-p <prefix>', { fallback: '建议你选择' })
+    .action(({ session, options }, ...args) => {
+      if (!args || args.length < 2)
+        return reply(session) + '请输入至少两个选项！'
+      return options?.prefix + args[randomInt(0, args.length - 1)]
     })
 }
