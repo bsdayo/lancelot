b7923c4e5b1765a2b035e65b00b49efea4360d92
b1acksoil <bs@blacksoil.top>
2022-03-20 15:00:31 +0800

    move prefix to template in /choose

--- src/plugins/utils/choose.ts
+++ src/plugins/utils/choose.ts
@@ -5,10 +5,15 @@ export default function enableChoose(ctx: Context) {
   ctx
     .command('choose <...args>', '随机选择')
     .shortcut('帮我选', { fuzzy: true })
-    .option('prefix', '-p <prefix>', { fallback: '建议你选择' })
+    .option('template', '-t <template>', { fallback: '建议你选择#' })
     .action(({ session, options }, ...args) => {
       if (!args || args.length < 2)
         return reply(session) + '请输入至少两个选项！'
-      return options?.prefix + args[randomInt(0, args.length - 1)]
+
+      const random = args[randomInt(0, args.length - 1)]
+      
+      return options?.template
+        .toString()
+        .replaceAll('#', random)
     })
 }
