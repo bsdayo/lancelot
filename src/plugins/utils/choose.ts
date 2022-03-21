362973e6b1f419f6eb84f13b51b3f5f59f30f4c7
b1acksoil <bs@blacksoil.top>
2022-03-21 21:13:50 +0800

    fix choose

--- src/plugins/utils/choose.ts
+++ src/plugins/utils/choose.ts
@@ -5,19 +5,21 @@ export default function enableChoose(ctx: Context) {
   ctx
     .command('choose <...args>', '随机选择')
     .shortcut('帮我选', { fuzzy: true })
-    .option('template', '-t <template>', { fallback: '建议你选择#', authority: 2 })
+    .option('template', '-t <template>', { authority: 2 })
     .action(({ session, options }, ...args) => {
+      const template: string = options?.template ?? '建议你选择#'
+
       if (!args || args.length < 2)
         return reply(session) + '请输入至少两个选项！'
 
-      if (options?.template.toString().replaceAll('#', '') === '')
+      if (template.toString().replaceAll(' ', '').replaceAll('#', '') === '')
         return reply(session) + '模板中仅存在占位符，请检查后重试。'
 
-      if (!options?.template.toString().includes('#'))
+      if (template.toString().includes('#'))
         return reply(session) + '模板中不存在占位符，请检查后重试。'
 
       const random = args[randomInt(0, args.length - 1)]
 
-      return options?.template.toString().replaceAll('#', random)
+      return template.toString().replaceAll('#', random)
     })
 }
