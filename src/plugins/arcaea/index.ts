6fde8c26b8b8fbed7dc248111b2b4f726d37379a
b1acksoil <bs@blacksoil.top>
2022-03-25 19:16:46 +0800

    update ignore rule

--- src/plugins/arcaea/index.ts
+++ src/plugins/arcaea/index.ts
@@ -78,7 +78,8 @@ export default {
     ctx.before('command/execute', ({ session, command }) => {
       if (
         config.ignoreSelfId?.includes(session?.selfId!) &&
-        command?.name.split('.')[0] === 'arc'
+        command?.name.split('.')[0] === 'arc' &&
+        ['b30', 'recent', 'best'].includes(command?.name.split('.')[1])
       ) {
         return '由于冻结过于频繁，本bot已停止提供Arcaea相关查询服务。\n您可以选择加入频道使用，详请进群744362693。'
       }
