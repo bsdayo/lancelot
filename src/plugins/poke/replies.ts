db10faba01627c60f2778d56538aabdbc29d1cd7
b1acksoil <bs@blacksoil.top>
2022-03-31 14:09:11 +0800

    update replies

--- src/plugins/poke/replies.ts
+++ src/plugins/poke/replies.ts
@@ -2,7 +2,14 @@ import { randomInt } from '../../utils'
 
 const replies: string[] = [
   '唔...啊？嗯！',
-  '嗯？找我有什么事吗？'
+  '嗯？找我有什么事吗？',
+  '舰长补给全保底，舰长副本零掉落 ≥ ≤',
+  '再戳就吃掉你的ptt！',
+  '再怎么戳我也不会属于你的 =w=',
+  '猜猜你要多久才能再看见这一条戳一戳',
+  '有笨蛋！',
+  'zzz',
+  '戳我就要给我买冰淇淋吃！'
 ]
 
 export default function getRandomReply(): string {
