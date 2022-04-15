51761f249abdcc8ce38d5f401ca6a7253d17e12b
b1acksoil <bs@blacksoil.top>
2022-04-15 17:19:51 +0800

    enable random

--- src/plugins/utils/index.ts
+++ src/plugins/utils/index.ts
@@ -1,11 +1,13 @@
 import { Context } from 'koishi'
 import enableChoose from './choose'
 import enableMainbot from './mainbot'
+import enableRandom from './random'
 
 export default {
   name: 'utils',
   apply(ctx: Context) {
     enableMainbot(ctx)
     enableChoose(ctx)
+    enableRandom(ctx)
   },
 }
